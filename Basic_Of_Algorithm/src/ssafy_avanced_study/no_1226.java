93d564d60df4e845c4630b323212497158091efe
monsileI <hansaem900d@kakao.com>
2022-07-31 23:16:20 +0900

    sw 역량테스트 bfs

--- Basic_Of_Algorithm/src/ssafy_avanced_study/no_1226.java
+++ Basic_Of_Algorithm/src/ssafy_avanced_study/no_1226.java
@@ -7,80 +7,90 @@ import java.util.LinkedList;
 
 public class no_1226 {
 	
-	static int answer = 0;
-	static int [][] move = {{1,0},{0,1},{-1,0},{0,-1}};
 	
 	public static void main(String[] args) throws Exception {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T;
-		T= Integer.parseInt(br.readLine());
+		
 		/*
 		   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
 		*/
-
-		for(int test_case = 1; test_case <= T; test_case++)
+		
+		int[][]move = {{1,0},{0,1},{-1,0},{0,-1}};
+		
+		for(int test_case = 1; test_case <= 10; test_case++)
 		{
+			
+			Integer.parseInt(br.readLine());
 			
 			int[][]map = new int[16][16];
 			
 			Queue<int []> q = new LinkedList<>();
 			
-			q.add(new int[] {0,0});
 			
 			int []start = new int[2];
 			int []end = new int[2];
+		
 			
 			for(int i=0;i<16;i++) {
-					String str =br.readLine();
+				String s = br.readLine();
 				for(int j=0;j<16;j++) {
-					map[i][j] = Integer.parseInt(""+str.charAt(j));
-					if(map[i][j]==2) {
-						start[0] = i;
-						start[1] = j;
-					}else if(map[i][j]==3) {
-						end[0] = i;
-						end[1] = j;
-					}
+					map[i][j] = Integer.parseInt(""+s.charAt(j));
+					
 				}
 			}
 			
+			StringBuilder sb = new StringBuilder();
 			
-			
-			int[][]temp = new int[16][16];
-			temp[start[0]][start[1]] = 0;
-			
-			
-			dfs(start[0],start[1],temp,end,map);
-			
-			
-			
-		}
+			boolean[][]visited = new boolean[16][16];
 		
-		
-	}
-	static void dfs(int i,int j,int[][]temp,int[]end,int[][]map) {
-		
-		
-		if(i==end[0] && j==end[1]) {
-			answer = 1;
-			return;
-		}
-		
-		for(int d=0;d<4;d++) {
-			int newI = i + move[d][0];
-			int newJ = j + move[d][1];
 			
-			if(newI<0||newJ<0||temp.length-1<newI||temp[0].length-1<newJ) continue;
+			q.add(new int[] {1,1});
+	
+			boolean isTrue = false;
 			
-			if(temp[newI][newJ]==0 && map[newI][newJ]==0) {
-				temp[newI][newJ] = temp[i][j] + 1;
-				dfs(newI, newJ, temp, end, map);
-				temp[newI][newJ] = 0;
+			while(!q.isEmpty()) {
+				
+				int [] c = q.poll();
+				int i = c[0];
+				int j = c[1];
+				
+	
+			
+				
+		
+				
+				for(int d=0;d<4;d++) {
+				
+					int ni = i+move[d][0];
+					int nj = j+move[d][1];
+					
+					if(ni<0||nj<0||15<ni||15<nj) continue;
+					
+					if(map[ni][nj]==3) {
+						isTrue = true;
+						break;
+					}
+					
+					if(!visited[ni][nj] && map[ni][nj]==0) {
+						visited[ni][nj] = true;
+						q.add(new int[] {ni,nj});
+						
+						
+					}
+					
+				}
+				
 			}
+			
+			sb.append("#").append(test_case).append(" ");
+			
+			if(isTrue) sb.append(1);
+			else sb.append(0);
+			
+			System.out.println(sb.toString());
+			
 		}
-		
-		
 		
 	}
 	
